8e4e910b255338fead4ada00cc45251a8688729a
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-24 13:20:44 +0100

    UPDATE: HERO COMP

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Hero from '@/components/Hero'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <main className="max-w-7xl mx-auto bg-[#0F1117]">
+        <Hero />
+        {children}
+        <Footer />
+        </main>
+        </body>
     </html>
   )
 }
