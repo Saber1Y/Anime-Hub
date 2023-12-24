67d0be5016a63b58a523b44af6f90b3e5fd1c9f6
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-24 17:30:31 +0100

    CREATE: FETCHED DATA

---
+++ app/action.js
@@ -0,0 +1,10 @@
+"use server"
+
+export const fetchAnime = async () => {
+    const res = await fetch('https://shikimori.one/api/animes');
+    const data = await res.json();
+
+    console.log(data)
+
+    return data;
+}
