7c803f8de962bc8848db79eedf6537265d3f89df
ayodeji <ayodejiakintobi1@gmail.com>
2023-12-28 18:36:54 +0100

    UPDATE: PAGE ADISPLAYED

--- components/Loadmore.jsx
+++ components/Loadmore.jsx
@@ -1,20 +1,34 @@
 'use client'
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useInView } from 'react-intersection-observer'
+import { fetchAnime } from "@/app/action";
+import AnimeCard from "./AnimeCard";
+
+
+let page = 2;
 
 function LoadMore() {
 
 const { ref, inView } = useInView();
+const [data, setData ] = useState([])
 
   useEffect(() => {
     if (inView) {
-      alert('end of row');
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      });
     }
   }, [inView])
 
   return (
     <>
+    <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item, index) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
           <Image
