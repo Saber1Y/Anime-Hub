8885f3e6d19a79640d82d2e98ad102113aef44e2
ayodeji <ayodejiakintobi1@gmail.com>
2024-03-18 18:57:15 +0100

    UPDATE: DATE ADDED

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 
 const Footer = () => {
+
+  const currentYear = new Date().getFullYear();
+
   return (
 <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
+      <p className="text-base font-bold text-white">{currentYear} EpicAnimeVault</p>
       <Image
         src="./logo.svg"
         alt="logo"
